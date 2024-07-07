48e9b0775a23b229296014901ceaa26db18e3e40
robinkii <littlerobin@bandgeek.dev>
2024-07-07 12:08:13 -0400

    1st iteration of cloneflix logo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App(){
     return(
         <>
             <div>
-                Homepage
+                <div className="border border-dark h-[86px] block pt-[1.5rem] pb-[1.5rem] m-auto pl-[3rem] pr-[3rem]">
+                    <h1 className="uppercase text-5xl font-martel text-[#ab34eb] cloneflix-logo">cloneflix</h1>
+                </div>
             </div>
         </>
     )
